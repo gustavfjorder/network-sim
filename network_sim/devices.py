0fc0b4c589c7b22c32102e2b97a546373bb58cef
tristanang <tangtzeheng@wesleyan.edu>
2018-10-28 21:28:01 -0700

    skeleton classes

---
+++ network_sim/devices.py
@@ -0,0 +1,18 @@
+class Host:
+    def __init__(self,name,port):
+
+    def addPort(self):
+        return
+
+    def send(self):
+        return
+
+class Router(algorith?):
+    def __init__(self):
+        self.queue = []
+
+class Packet:
+    def __init__(self,destination,information):
+        self.destination = destination
+        self.information = information
+        self.size = len(information)
