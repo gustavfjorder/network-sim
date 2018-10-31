4fb4c4fdb59057dc25f3ec7d09d2ae622a77f2b2
slcoplin <sophiacoplin@gmail.com>
2018-10-30 18:20:45 -0700

    Add Link class and Questions for TA

--- notes.py
+++ notes.py
@@ -11,16 +11,26 @@ class Router(algorith?):
     def __init__(self):
         self.queue = []
 
+    def receivePacket(packet)
+
 class Packet:
     def __init__(self,destination,information):
         self.destination = destination
         self.information = information
         self.size = len(information)
 
+class Link:
+    def __init__():
+        self.capacity = 0
+
+    def receivePacket(packet):
+        # wait the amount of time it would take to transmit the packet
+        # add the packet to the Link
+
 #where do we 'store' the packets which are waiting in the link...?
 #consensus is that we will store it in the link
 """
-A -L1-> R1 -L2-> R2 -L3-> B        
+A -L1-> R1 -L2-> R2 -L3-> B
 At any given time the packet is owned by one of the objects above
 
 A gives packet to L1
@@ -29,5 +39,4 @@ R1 gives packets to L2... etc
 
 packet = Packet(...)
 A.send(packet) information in packet directs it to L1
-
 """
