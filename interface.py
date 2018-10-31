0d3433b40ae77c8d94325fb01fda52ca4471c6d3
Gustav <gfjorder@caltech.edu>
2018-10-30 19:53:51 -0700

    add interface and test0.json

--- interface.py
+++ interface.py
@@ -1,5 +1,20 @@
-import notes
+#import notes
+import json
+from pprint import pprint
+
+
 
 def get_config(filename):
 # Takes filename (json) file and outputs a list of hosts, routers, links, flows.
 # The lists are lists of objects.
+
+    with open(filename) as f:
+        data = json.load(f)
+
+    pprint(data)
+
+def main():
+    get_config("test0.json")
+
+if __name__ == "__main__":
+    main()
