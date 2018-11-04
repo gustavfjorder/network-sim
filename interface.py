63093f6311c0e0dacb5fc476fb0b3d3b95e05fa0
Gustav Fjorder <gfjorder@caltech.edu>
2018-11-03 17:42:25 -0700

    make hosts, links, flows into objects

--- interface.py
+++ interface.py
@@ -1,21 +1,50 @@
 #import notes
 import json
 
-def get_config(filename):
+def get_config(env,filename):
 # Takes filename (json) file and outputs a list of hosts, routers, links, flows.
     try:
         with open(filename) as f:
-            data = json.load(f)
+            test_data = json.load(f)
     except FileNotFoundError as error:
         print("File not found")
-        data = None
-    return data
+        test_data = None
+        return
+
+    hosts = []
+    links = []
+    flows = []
+    for host in test_data['hosts']:
+        host = Host(env, test_data['hosts'][host]['host_id'],1)
+        hosts.append(host)
+
+    # create link objects
+    for link in test_data["links"]:
+        link = Link(env, \
+        test_data['links'][link]['link_id'],\
+        test_data['links'][link]['link_delay'], \
+        test_data['links'][link]['link_buffer'], \
+        test_data['links'][link]['link_rate'])
+        links.append(link)
+
+    # create flow objects
+    for flow in test_data['flows']:
+        flow = Flow(\
+        test_data['flows'][flow]['flow_src'],\
+        test_data['flows'][flow]['flow_dest'],\
+        test_data['flows'][flow]['data_amt'],\
+        test_data['flows'][flow]['flow_start'])
+        flows.append(flow)
+
+    return hosts,links,flows
+
 
 def main():
-    input_file = input("Test name: ")
-    test_data = get_config(input_file)
-    if test_data != None:
-        print(test_data)
+    env = 0 
+    input_file ="test0.json"#{} input("Test name: ")
+    test_data = get_config(env,input_file)
+
+
 
 if __name__ == "__main__":
     main()
