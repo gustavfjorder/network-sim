229059a6a352d7cea754ee3d6505b22854df895d
tristanang <tangtzeheng@wesleyan.edu>
2018-11-04 20:33:34 -0800

    completetd packets and missing functions in flow

--- network_sim/flow.py
+++ network_sim/flow.py
@@ -1,3 +1,7 @@
+from packets import Packet
+
+data_size = 1024
+
 class Tahoe:
     """
     #Implementation of Go Back N
@@ -6,18 +10,34 @@ class Tahoe:
         self.env = env
         self.source = source
         self.destination = destination
-        self.packets = makePackets(size) #expecting a indexable list as implementation
+        self.packets = self.makePackets(size) #expecting a indexable list as implementation
         self.num_packets = len(self.packets)
         self.done = 0
         self.windowSize = windowSize
         self.ackTimeOut = ackTimeOut
         self.windowIndex = (0, min(self.windowSize - 1, self.num_packets - 1)) #no zero indexing here
 
+    def makePackets(size):
+        size = size * 1024 * 1024# In bytes
+        N = size / data_size
+
+        output = []
+        
+        for i in range(N):
+            output.append(Packet(self.source,self.destination,i+1, 'Data', data_size))
+
+        return output
+
+    def packetProcess(packet):
+        assert packet.type == 'ACK'
+        return packet.ackData['Tahoe'] # should be an int
+
+
     def setWindow(start):
         self.windowIndex = (start - 1, min(nextExpectedPacketNumber - 1 + self.windowSize - 1, self.num_packets - 1))
 
     def put(self, packet): #receiving
-        nextExpectedPacketNumber = packetProcess(packet)
+        nextExpectedPacketNumber = self.packetProcess(packet)
         
         if nextExpectedPacketNumber > self.num_packets:
             self.done = 1
