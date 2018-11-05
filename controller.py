2c1ff96335371e21b1666e954debdd312b2d40d3
slcoplin <sophiacoplin@gmail.com>
2018-11-04 20:57:53 -0800

    Restructure

--- controller.py
+++ controller.py
@@ -1,16 +1,13 @@
 import simpy
 import interface
+from monitor import Monitor, show_results
 
-# For graphing at the end
-import matplotlib.pyplot as plt
 
 def runSimulator(input_file):
 
     env = simpy.Environment()
 
     hosts, links, flows = interface.get_config(env, input_file)
-
-    print("worked")
 
     for link in links:
         env.process(link.run())
@@ -25,62 +22,3 @@ def runSimulator(input_file):
 
     # Graph the results
     show_results(monitor)
-
-
-class Monitor:
-    def __init__(self, links, flows):
-        self.links = links
-        self.flows = flows
-        self.refreshRate = 10  # ms
-
-        # Initialize a place to store link info
-        self.linkRates = [[] for i in range(len(links))]
-        self.linkBufferUsed = [[] for i in range(len(links))]
-        self.linkPacketsDropped = [[] for i in range(len(links))]
-
-        # Initialize flow info
-        self.flowWindowSize = [[] for i in range(len(flows))]
-
-    def run():
-        while True:
-            self.checkLinks()
-            self.checkFlows()
-
-            # Wait another ms, then check again
-            yield simpy.timeout(self.refreshRate)
-
-    def checkLinks():
-        for i, link in enumerate(links):
-            # Get the number of bits sent recently, then reset
-            linkRate = link.bitsSent / (self.refreshRate * 10**-3) # bps
-            link.bitsSent = 0
-            linkRates[i].append(linkRate)
-
-            bufferUsed = link.bufferUsed
-            linkBufferUsed[i].append(bufferUsed)
-
-            # Get the number of packets dropped, then reset
-            packetsDropped = link.packetsDropped
-            link.packetsDropped = 0
-            self.linkPacketsDropped[i].append(packetsDropped)
-
-    def checkFlows():
-        for i, flow in enumerate(flows):
-            # Get the current window size
-            windowSize = flow.windowSize
-            flowWindowSize[i].append(windowSize)
-
-
-
-
-def show_results(monitor):
-    for i, link in enumerate(monitor.links):
-        plt.plot(monitor.linkRates[i])
-        plt.show()
-
-        plt.plot(monitor.linkBufferUsed[i])
-        plt.show()
-
-    for i, flows in enumerate(monitor.flows):
-        plt.plot(monitor.flowWindowSize[i])
-        plt.show()
--- notes.py
+++ notes.py
@@ -16,26 +16,36 @@ import collections
 # need to discuss this. Look at page 92 of WP
 class Host:
     def __init__(self,env,name,port):
-        self.link = None
+        self.env = env
+        self.id = name
+        addLink(link)
         self.flows = []
 
-    #def addPort(self):
-        #return
+    def addFlow(flow):
+        self.flows.append(flow)
+
+    def addLink(link):
+        self.link = link
 
     def send(self, packet):
         # From flow to link
+        self.link.put(packet)
         return
 
     def put(self, packet):
         # Receive a packet from link
         # Pass it to flow
         # receive ack OR send ack if it isn't an ack
+        if(packet.type == 'ACK'):
+            pass
+        else:
+            # Packet is not an Awknoledgement, need to  send an awknowledgement
+            # new destination is the source, get this from the flow
         pass
 
     def run(self):
         while (True):
             # if there is a flow
-
                 # send a packet
                 pass
 
@@ -58,91 +68,6 @@ class Router:
             # If there are packets to send, send them
             pass
 
-class Packet:
-    def __init__(self,env,destination,information):
-        self.destination = destination
-        self.information = information
-        self.size = len(information)   # bytes
-
-        # something that the lecture notes say each packet has in the header
-        # to discuss
-        self.sequenceNumber = None
-        self.ackNumber = None
-
-class Link:
-    def __init__(self, env, id, delay, bufferSize, rate):
-        self.id = id
-        self.env = env
-        self.rate = rate               # Mbps
-        # An infinite size queue. We internally enforce limits.
-        self.buffer = collections.deque()
-        self.propagationDelay = delay  # ms
-        self.bufferSize = bufferSize   # bytes
-        self.bufferUsed = 0;           # bytes
-        self.source = None             # a host or router
-        self.destination = None        # a host or router
-
-        # For monitoring
-        self.bitsSent = 0
-        self.packetsDropped = 0
-
-    # This is a generator, not a function so must be called as such
-    # or created as a simpy process.
-    def put(self, packet):
-        # Receives a packet.
-        #
-        # Put the packet in the buffer. Then wait the transmissionTime for the
-        # packet to finish.
-        #
-        # Warning: This should not get called by two things, because the
-        # transmission delays will get messed up, probably.
-        #
-        # The generator for Link will deal with the link's side of transmission
-        # delay and propagation delay.
-
-        # Put into the buffer (run deals with transmission delay, so do this
-        # first) if buffer not full. Otherwise, drop the packet.
-        if self.bufferUsed + packet.size <= self.bufferSize:
-            self.buffer.append(packet)
-            self.bufferUsed += packet.size
-        else: # Drop the packet
-            self.packetsDropped += 1
-
-        # Wait transmissionTime of the packet, to hold back source.
-        transmissionDelay = packet.size * 8 / self.rate   # 8 for byte to bit
-        yield self.env.timeout(transmissionDelay) # Not sure if yield is right
-
-    # This is a generator, not a function so must be called as such
-    # or created as a simpy process.
-    # Warning: relies on host or router 'put' is a generator, not a function
-    def run(self):
-        while True:
-            # If buffer not empty, send all the packets in buffer.
-            # After propagation delay (plus transmission), they will arrive.
-            # Space departures by transmission delay.
-            while self.buffer:
-                # Get packet (popleft is FIFO)
-                packet = self.buffer.popleft()
-
-                # Wait transmission delay
-                # 8 for byte to bit
-                transmissionDelay = packet.size * 8 / self.rate
-                yield self.env.timeout(transmissionDelay)
-                # The packet has been sent, so buffer has been freed
-                self.bufferUsed -= packet.size
-
-                # Monitoring
-                self.bitsSent += packet.size
-
-                # Pass to router after propagationDelay time (but don't
-                # wait).
-                # start_delayed is like process, but will start the process
-                # after a time delay.
-                simpy.util.start_delayed(self.env, \
-                                     self.destination.put(packet), \
-                                     self.propagationDelay)
-            # Check again after 0.1 ms.
-            yield self.env.timeout(0.1)
 
 
 def addFlow(flow):
