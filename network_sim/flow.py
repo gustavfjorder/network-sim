109b5151623d967611027a8e1603338034ac85c4
tristanang <tangtzeheng@wesleyan.edu>
2018-11-04 19:49:11 -0800

    tahoe is complete

--- network_sim/flow.py
+++ network_sim/flow.py
@@ -1,46 +1,3 @@
-"""
-Flow(Tahoe):
-    def __init__(self,env,source,destination,size):
-        self.size = size #for now size is in packets
-        self.source = source
-        #self.destination = destination #useless variable for now
-
-
-
-    def run(self,start=1.0):
-        while True:
-
-            yield
-
-    def receive(self, packet):
-        value = self.packetProcess(packet)
-
-        # Receive an acknow
-
-    def windowControl
-
-#class Algorithm:
-
-#class Reno(Tahoe):
-
-class Tahoe:
-    """
-    #Implementation of Go Back N
-    """
-    def __init__(self,windowSize,ackTimeOut):
-        self.windowSize = windowSize
-        self.ackTimeOut = ackTimeOut
-        self.windowIndex = (0, self.windowSize - 1) #no zero indexing here
-        self.send = 
-
-    def receive(self, nextExpectedPacketNumber):
-        self.windowIndex = (nextExpectedPacketNumber - 1, nextExpectedPacketNumber - 1 + self.windowSize - 1)
-        # set windowIndex based on packetnumber
-
-    def failed(self):
-        #send all packets in window
-
-"""
 class Tahoe:
     """
     #Implementation of Go Back N
@@ -49,33 +6,43 @@ class Tahoe:
         self.env = env
         self.source = source
         self.destination = destination
-        self.packets = [i for i in range(1,size + 1)]
+        self.packets = makePackets(size) #expecting a indexable list as implementation
+        self.num_packets = len(self.packets)
         self.done = 0
         self.windowSize = windowSize
         self.ackTimeOut = ackTimeOut
-        self.windowIndex = (0, self.windowSize - 1) #no zero indexing here
+        self.windowIndex = (0, min(self.windowSize - 1, self.num_packets - 1)) #no zero indexing here
 
-    def receive(self, packet):
+    def setWindow(start):
+        self.windowIndex = (start - 1, min(nextExpectedPacketNumber - 1 + self.windowSize - 1, self.num_packets - 1))
+
+    def put(self, packet): #receiving
         nextExpectedPacketNumber = packetProcess(packet)
-        self.windowIndex = (nextExpectedPacketNumber - 1, nextExpectedPacketNumber - 1 + self.windowSize - 1)
-        # set windowIndex based on packetnumber
+        
+        if not nextExpectedPacketNumber:
+            self.done = 1
+        else:
+            setWindow(nextExpectedPacketNumber)
 
-    def put(self):
+    def send(self,source):
         start, end = self.windowIndex[0], self.windowIndex[1]
         for i in range(start, end + 1):
-            #add thing into queue???
-        #send all packets in window
+            source.put(self.packets[i])
+
+    def timeOut(self, time=self.ackTimeOut):
+        yield self.env.timeout(time)
 
     def run(self):
         while not self.done:
             self.put()
             try:
                 yield self.env.process(self.env.timeout(self.ackTimeOut))
-                self.put() #retransmit
-            except simpy.Interrupt:
-                receive(Packet) #how do we transfer the acknowledgement packet here!
 
-# still figuring out
-def ack(env, car):
-    car.receive()
+            except simpy.Interrupt: #receive ACK
+                pass
+                #print('Got an acknowledgement :)')
+
+# This should be what the host uses to interrupt me sortaa
+def ack(env, flow):
+    car.put(ackPacket)
     car.action.interrupt()
