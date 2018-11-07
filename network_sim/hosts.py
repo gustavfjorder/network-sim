4e229c0f3395915b4e1808ef12ba5231a6fb3105
slcoplin <sophiacoplin@gmail.com>
2018-11-06 22:37:49 -0800

    hosts

--- network_sim/hosts.py
+++ network_sim/hosts.py
@@ -1,4 +1,4 @@
-from packets import Packet
+from packets import ACK
 
 class Host:
     def __init__(self,env, name, link):
@@ -29,7 +29,7 @@ class Host:
             # Packet is not an Acknowledgement, need to  send an acknowledgement
             # new destination is the source, get this from the flow\
             ackData = None  # initalize this later
-            ackPacket = Packet(self, packet.destination, packet.source, \
+            ackPacket = ACK(packet.destination, packet.source, \
                 packet.sequenceNumber, ackData )
             self.link.put(ackPacket)
 
